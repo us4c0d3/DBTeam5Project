053dbeb62cd657968c248d67d319f31e7f546ad5
us4c0d3 <jwseok312@gmail.com>
2023-11-09 19:43:28 +0900

    init: set jdbc connection

---
+++ Phase3/src/phase3/Main.java
@@ -0,0 +1,48 @@
+package phase3;
+
+import java.sql.*; // import JDBC package
+
+
+public class Main {
+	public static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+	public static final String USER_NAME ="teamproject"; 
+	public static final String USER_PASSWD ="comp322";
+		
+	public static void main(String[] args) {
+		Connection conn = null; // Connection object
+		Statement stmt = null;	// Statement object
+		String sql = ""; // an SQL statement 
+		try {
+			// Load a JDBC driver for Oracle DBMS
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// Get a Connection object 
+			System.out.println("Success!");
+		}catch(ClassNotFoundException e) {
+			System.err.println("error = " + e.getMessage());
+			System.exit(1);
+		}
+
+		// Make a connection
+		try{
+			conn = DriverManager.getConnection(URL, USER_NAME, USER_PASSWD); 
+			System.out.println("Connected.");
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+			System.err.println("Cannot get a connection: " + ex.getLocalizedMessage());
+			System.err.println("Cannot get a connection: " + ex.getMessage());
+			System.exit(1);
+		}
+		
+		// Release database resources.
+		try {
+			// Close the Statement object.
+			stmt.close(); 
+			// Close the Connection object.
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
