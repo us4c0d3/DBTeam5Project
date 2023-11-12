70a856e7494d2b0ec4529329f26f53cd56d1c1b4
us4c0d3 <jwseok312@gmail.com>
2023-11-12 17:26:26 +0900

    phase3

---
+++ Phase3/Team5-README.txt
@@ -0,0 +1,72 @@
+Eclipse IDE를 사용하였고 Java 11, ojdbc10.jar를 사용했습니다.
+
+Main.java에서 실행을 하면 
+
+0. Exit
+1. Query
+2. DML
+Select Type:
+
+위와 같은 메뉴가 나올 것이고 0은 종료, 1은 쿼리문 출력을 위한 옵션 선택, 2는 DML 출력을 위한 옵션 선택이 출력됩니다.
+
+1 입력 시 Phase2의 쿼리 번호를 선택하도록 아래와 같이 출력됩니다.
+Query: 2, 3, 4, 6, 7, 9, 12, 13, 17, 18, 19, 20
+Select Query Type: 
+
+쿼리 번호를 입력할 시 각 쿼리별 입력받을 attribute의 이름이 출력되고 입력받습니다.
+
+쿼리 설명
+Phase2 제출물 중 Q7, Q9, Q18, Q20 쿼리문을 수정하였습니다.
+Q2: payment_type을 입력받고 customer의 이름과 휴대폰 번호를 출력합니다. "Cash" 또는 "Credit Card"를 입력받습니다.
+Q3: 몇 개 이하의 재료를 사용한 menu_item의 이름과 재료 개수를 출력합니다. 정수를 입력받습니다. 
+Q4: 품절 여부에 따른 menu_item을 주문한 customer의 customer_id와 이름을 출력합니다. 품절 여부를 "T" 또는 "F"로 입력받습니다.
+Q6: 특정 기간에 게시된 menu에 등록됐던 menu_item의 이름을 출력합니다. 게시 시작 날짜와 종료 날짜를 yymmdd형식으로 입력받습니다.
+Q7: menu_item의 이름을 입력 받고 menu_item의 이름과 menu_item의 수량, menu_item에 필요한 재료의 총 수량을 출력합니다. menu_item의 이름은 "Spicy Paneer Wrap", "American Veg Burger", "Veg Maharaja Mac", "Green Chilli Aloo Naan", "Pizza Puff" 등이 있습니다. 
+Q9: menu_item의 이름을 입력 받고 그 menu_item을 관리하는 manager의 이름과 그 menu_item이 몇 번 주문되었는지의 총 수량을 출력합니다. menu_item의 이름 종류는 Q7과 같습니다. 
+Q12: customer_id를 입력받고 그 사람이 지금까지 주문한 메뉴 항목의 총 가격을 계산하고, 그 결과를 order별로 그룹화하여 출력합니다. customer_id는 "CU" + 숫자 6개로 구성되어있습니다.
+Q13: menu_item의 category를 입력받고, 그 카테고리에 속하는 menu_item의 단일가격 평균보다 총합 가격이 더 큰 payment의 order_id, customer_id, 그리고 총합 가격을 출력합니다. category는 "Gourmet Menu", "Breakfast Menu", "Beverages Menu", "Regular Menu", "Desserts Menu", "McCafe Menu", "Condiments Menu"가 있습니다.
+Q17: 정수 n을 입력받아 각 menu_item중 가장 많이 주문된 상위 n개를 출력합니다.
+Q18: 정수 n을 입력받아 총 가격이 n 이상인 payment에서 customer의 이름, 주문한 menu_item의 이름 그리고 해당 menu_item을 관리하는 manager의 이름을 출력합니다. 
+Q19: 정수 n을 입력받아 동일한 카테고리에 속하는 menu_item을 n개 이상 관리하는 manager의 이름과 휴대폰 번호를 이름의 오름차순으로 출력합니다.
+Q20: customer_id를 입력받고 customer가 주문한 menu_item의 이름과 해당 menu_item을 만드는 chef의 이름을 출력합니다. 
+
+쿼리문의 수정사항은 아래와 같습니다. 
+Q7. 
+각 menu_item의 이름과 menu_item의 수량, menu_item에 필요한 재료의 총 수량을 출력합니다.
+-> 이름이 "Spicy Paneer Wrap"인 menu_item의 이름과 menu_item의 수량, menu_item에 필요한 재료의 총 수량을 출력합니다.
+
+Q9. 
+각 menu_item에 대해 그것을 관리하는 manager의 이름과 그 menu_item이 몇 번 주문되었는지의 총 수량을 출력합니다
+-> 이름이 "Spicy Paneer Wrap"인 menu_item에 대해 그것을 관리하는 manager의 이름과 그 menu_item이 몇 번 주문되었는지의 총 수량을 출력합니다.
+
+Q18. 
+payment에서 customer의 이름, 주문한 menu_item의 이름 그리고 해당 menu_item을 관리하는 manager의 이름을 출력합니다.
+-> 총 가격이 100000 이상인 payment에서 customer의 이름, 주문한 menu_item의 이름 그리고 해당 menu_item을 관리하는 manager의 이름을 출력합니다.
+
+Q20. 
+customer가 주문한 menu_item의 이름과 해당 menu_item을 만드는 chef의 이름을 출력합니다. 결과는 chef의 이름 순으로 정렬합니다.
+-> customer_id가 "CU000001" 인 customer가 주문한 menu_item의 이름과 해당 menu_item을 만드는 chef의 이름을 출력합니다. 결과는 chef의 이름 순으로 정렬합니다.
+
+수정된 쿼리를 포함하여 사용된 모든 쿼리문은 첨부된 using_query.sql에 있습니다.
+
+
+메인에서 2 입력시 DML 클래스가 실행됩니다.
+DML: Customer, Manager, Chef 중 어떤 엔티티에 대한 DML을 선택할 것인지 입력받은 후 각각의 정보 입력, 수정, 삭제를 하는 기능을 선택합니다.
+
+1. Customer: Customer에 대한 정보 입력, 수정, 삭제를 수행합니다. 
+    1. Customer 정보 입력 : Customer로부터 이름과 비밀번호 그리고 휴대폰 번호를 입력받는다. 신규 Customer id는 기존에 순차적으로 저장되어 있는 Customer_id에서 가장 큰 id 번호에 1을 더해 생성한다. 최종적으로 customer의 id, 이름, 비밀번호, 휴대폰 번호를 추가하는 insert를 실행한다.
+    2. Customer 정보 수정 : Customer로부터 id를 입력 받고, 이름과 비밀번호 그리고 휴대폰 번호 중 수정할 항목을 선택하게 한다. customer가 선택한 수정 항목에 대해 새로운 값을 입력 받게 하고 Update를 실행한다.
+    3. Customer 정보 삭제 : Customer로부터 id를 입력 받고, customer id에 해당하는 정보를 삭제하는 delete를 실행한다.
+
+2. Manager: Manager에 대한 정보 입력, 수정, 삭제를 수행합니다.
+    1. Manager 정보 입력 : Manager로부터 이름과 비밀번호 그리고 휴대폰 번호를 입력받는다. 신규 Manager id는 기존에 순차적으로 저장되어 있는 Manager_id에서 가장 큰 id 번호에 1을 더해 생성한다. 최종적으로 manager의 id, 이름, 비밀번호, 휴대폰 번호를 추가하는 insert를 실행한다.
+    2. Manager 정보 수정 : Manager로부터 id를 입력 받고, 이름과 비밀번호 그리고 휴대폰 번호 중 수정할 항목을 선택하게 한다. manager가 선택한 수정 항목에 대해 새로운 값을 입력 받게 하고 Update를 실행한다.
+    3. Manager 정보 삭제 : Manager로부터 id를 입력 받고, manager id에 해당하는 정보를 삭제하는 delete를 실행한다.
+
+3. Chef: Chef에 대한 정보 입력, 수정, 삭제를 수행합니다.
+    1. Chef 정보 입력 : chef로부터 이름과 휴대폰 번호를 입력받는다. 신규 Chef id는 기존에 순차적으로 저장되어 있는 Chef_id에서 가장 큰 id 번호에 1을 더해 생성한다. 최종적으로 chef의 id, 이름, 휴대폰 번호를 추가하는 insert를 실행한다.
+    2. Chef 정보 수정 : chef로부터 id를 입력 받고, 이름과 휴대폰번호 중 수정할 항목을 선택하게 한다. chef가 선택한 수정 항목에 대해 새로운 값을 입력 받게 하고 Update를 실행한다.
+    3. Chef 정보 삭제 : Chef로 부터 id를 입력 받고, Chef id에 해당하는 정보를 삭제하는 delete를 실행한다.
+
+
+Phase2대비 ERD와 Relational schema가 일부 수정되었습니다. 로그인을 가정하고 Customer와 Manager 엔티티에 ID 속성이 있었으나 customer_id와 manager_id가 그 역할을 대체할 수 있다고 생각하여 두 엔티티에서 ID 속성을 삭제하였습니다. 수정된 사항이 포함된 DDL 및 Insert문은 각각 Team5-Phase2-1-modified.sql, Team5-Phase2-2-modified.sql로 첨부하였습니다.
---
+++ Phase3/using_query.sql
@@ -0,0 +1,143 @@
+--Q2 - type2
+-- payment 중에서 payment_type이 'Cash'인 customer의 이름과 폰번호를 중복없이 출력
+SELECT distinct cu.name, cu.phone_number
+FROM customer cu, payment p
+WHERE cu.customer_id = p.customer_id
+AND p.payment_type = 'Cash';
+
+--Q3 - type3
+-- menu_item 중에서 ingredient를 5개 이하로 사용한 menu_item의 이름과 그 menu_item의 ingredient 개수을 출력
+SELECT mi.name, COUNT()
+FROM menu_item mi, ingredient i, need n
+WHERE n.item_id = mi.item_id
+AND n.ingredient_id = i.ingredient_id
+GROUP BY mi.name, mi.item_id
+HAVING COUNT() <= 5;
+
+--Q4 - type4
+-- 현재 soldout된 menu_item을 주문했던 customer의 customer_id와 이름을 출력
+SELECT cu.customer_id, cu.name
+FROM customer cu
+WHERE cu.customer_id IN (
+    SELECT po.customer_id
+    FROM menu_item mi, part_of po
+    WHERE mi.item_id = po.item_id
+    AND mi.soldout = 'T');
+
+--Q6 - type6
+-- menu_item 중에서 2023.01.01 ~ 2023.12.31까지 게시된 menu에 포함된 menu_item의 이름을 출력
+SELECT mi.name
+FROM menu_item mi
+WHERE (mi.item_id) IN (
+    SELECT co.item_id
+    FROM contains co, menu me
+    WHERE co.menu_id = me.menu_id
+    AND me.start_date >= TO_DATE('20230101')
+    AND me.end_date <= TO_DATE('20231231'));
+
+
+--Q12 - type9
+-- customer_id가 'CU000001'인 사람이 지금까지 주문한 메뉴 항목의 총 가격을 계산하고, 그 결과를 order별로 그룹화하여 출력
+SELECT p.order_id, SUM(mi.unit_price * po.amount) AS total_order_price
+FROM customer c
+JOIN PAYMENT p ON c.customer_id = p.customer_id
+JOIN part_of po ON p.order_id = po.order_id AND p.customer_id = po.customer_id
+JOIN menu_item mi ON po.item_id = mi.item_id
+WHERE c.customer_id = 'CU000001'
+GROUP BY p.order_id
+ORDER BY p.order_id;
+
+--Q13 - type4
+-- menu_item의 category가 'Breakfast Menu'인 menu_item의 unit_price 평균보다 total_price가 더 큰 payment의 order_id, customer_id, total_price를 출력
+SELECT p.order_id, p.customer_id, p.total_price
+FROM PAYMENT p
+WHERE p.total_price > (
+    SELECT AVG(mi.unit_price)
+    FROM menu_item mi
+    WHERE mi.category = 'Breakfast Menu'
+);
+
+
+
+--Q17 - type7
+-- 각 menu_item별로 몇 번 주문되었는지 확인하고, 가장 많이 주문된 상위 5개의 menu_item의 이름과 주문 횟수를 출력
+SELECT inlview.item_name, inlview.order_count
+FROM (
+    SELECT mi.name AS item_name, COUNT(p.item_id) AS order_count
+    FROM menu_item mi
+    LEFT JOIN part_of p ON mi.item_id = p.item_id
+    GROUP BY mi.name
+) inlview
+ORDER BY inlview.order_count DESC
+FETCH FIRST 5 ROWS ONLY;
+
+
+
+
+--Q19 - type6
+-- 같은 category의 menu_item을 3개 이상 관리하는 manager의 이름과 phone_number를 이름의 오름차순으로 출력
+SELECT ma.name, ma.phone_number
+FROM manager ma
+WHERE ma.manager_id IN (
+    SELECT mg.manager_id
+    FROM managed_item mg, menu_item mi
+    WHERE mi.item_id = mg.item_id
+    GROUP BY mg.manager_id, mi.category
+    HAVING COUNT(*) >= 3)
+ORDER BY ma.name;
+
+-------------------------------------------------------------------------
+
+--Q7 - type7
+-- 각 menu_item의 이름과 menu_item의 수량, menu_item에 필요한 재료의 총 수량을 출력
+-- -> 이름이 "Spicy Paneer Wrap"인 menu_item의 이름과 menu_item의 수량, menu_item에 필요한 재료의 총 수량을 출력
+SELECT mi.name, mi.item_quantity, iv.total_ingredient_quantity
+FROM menu_item mi
+JOIN (
+    SELECT n.item_id, SUM(i.quantity) AS total_ingredient_quantity
+    FROM need n, ingredient i
+    WHERE n.ingredient_id = i.ingredient_id
+    GROUP BY n.item_id
+) iv ON mi.item_id = iv.item_id
+WHERE mi.name = "Spicy Paneer Wrap"; -- 수정부분
+
+
+--Q9 - type9
+-- 각 menu_item에 대해 그것을 관리하는 manager의 이름과 그 menu_item이 몇 번 주문되었는지의 총 수량을 출력
+-- -> 이름이 "Spicy Paneer Wrap"인 menu_item에 대해 그것을 관리하는 manager의 이름과 그 menu_item이 몇 번 주문되었는지의 총 수량을 출력
+SELECT m.name AS manager_name, mi.name AS menu_item_name, SUM(po.amount) AS total_ordered
+FROM menu_item mi
+JOIN managed_item mani ON mi.item_id = mani.item_id
+JOIN manager m ON mani.manager_id = m.manager_id
+LEFT JOIN part_of po ON mi.item_id = po.item_id
+WHERE mi.name = "Spicy Paneer Wrap" -- 수정부분
+GROUP BY m.name, mi.name
+ORDER BY m.name, total_ordered DESC;
+
+
+--Q18 - type2
+-- payment에서 customer의 이름, 주문한 menu_item의 이름 그리고 해당 menu_item을 관리하는 manager의 이름을 출력
+-- -> 총 가격이 100000 이상인 payment에서 customer의 이름, 주문한 menu_item의 이름 그리고 해당 menu_item을 관리하는 manager의 이름을 출력
+SELECT c.name AS customer_name, mi.name AS menu_item_name, m.name AS manager_name
+FROM customer c, payment p, part_of po, menu_item mi, managed_item mani, manager m
+WHERE c.customer_id = p.customer_id
+AND p.order_id = po.order_id
+AND po.item_id = mi.item_id
+AND mi.item_id = mani.item_id
+AND mani.manager_id = m.manager_id
+AND p.Total_price >= 100000; -- 수정부분
+
+
+--Q20 - type8
+-- customer가 주문한 menu_item의 이름과 해당 menu_item을 만드는 chef의 이름을 출력. 결과는 chef의 이름 순으로 정렬
+-- -> customer_id가 "CU000001" 인 customer가 주문한 menu_item의 이름과 해당 menu_item을 만드는 chef의 이름을 출력. 결과는 chef의 이름 순으로 정렬
+SELECT c.name AS customer_name, mi.name AS menu_item_name, ch.name AS chef_name
+FROM customer c, payment p, part_of po, menu_item mi, chef ch
+WHERE c.customer_id = p.customer_id
+AND p.order_id = po.order_id
+AND po.item_id = mi.item_id
+AND p.chef_id = ch.chef_id
+AND c.customer_id = "CU000001" -- 수정부분
+ORDER BY ch.name;
+
+
