54baa603861adfd33d6afaeae8d9fe564ab694bf
us4c0d3 <jwseok312@gmail.com>
2023-11-22 21:30:15 +0900

    feat: add Payment table delete query feature

---
+++ Phase4/src/main/java/table/Payment.java
@@ -0,0 +1,40 @@
+package table;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Payment {
+	private Connection conn;
+	PreparedStatement ps;
+	String sql = "";
+
+	private Payment(Connection conn) {
+		this.conn = conn;
+	}
+
+	public ResultSet INSERT() {
+
+	}
+
+	public int UPDATE(String updateAttribute, String updateValue, String updateId) {
+
+	}
+
+	public int DELETE(String deleteId) {
+		int res = -1;
+		try {
+			sql = "DELETE FROM Payment WHERE order_id = ?";
+			ps = this.conn.prepareStatement(sql);
+			ps.setString(1, deleteId);
+
+			res = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		ps.close();
+		return res;
+	}
+}
