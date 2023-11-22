c1c22bf0557bb5f42ec445b5bbbff77b972df8f3
us4c0d3 <jwseok312@gmail.com>
2023-11-22 21:30:37 +0900

    feat: add Payment table update query feature

--- Phase4/src/main/java/table/Payment.java
+++ Phase4/src/main/java/table/Payment.java
@@ -19,7 +19,20 @@ public class Payment {
 	}
 
 	public int UPDATE(String updateAttribute, String updateValue, String updateId) {
+		int res = -1;
+		try {
+			sql = "UPDATE PAYMENT SET ? = ? WHERE order_id = ?";
+			ps = this.conn.prepareStatement(sql);
+			ps.setString(1, updateAttribute);
+			ps.setString(2, updateValue);
+			ps.setString(3, updateId);
 
+			res = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return res;
 	}
 
 	public int DELETE(String deleteId) {
