6f07cbeabe23ae06dd7d3d3aca5de464c910e3f9
lnsqj123 <holmane33@gmail.com>
2023-11-23 21:45:22 +0900

    feat: Login을 위한 SELECT 함수 추가, IOException 제거

--- Phase4/src/main/java/com/app/tables/Manager.java
+++ Phase4/src/main/java/com/app/tables/Manager.java
@@ -1,115 +1,127 @@
-package com.app.tables;
+package table;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Manager {
-	Connection conn;
-	String sql = "";
-	PreparedStatement ps;
+  Connection conn;
+  String sql = "";
+  PreparedStatement ps;
 
-	public Manager(Connection conn) {
-		this.conn = conn;
-	}
+  public Manager(Connection conn) {
+    this.conn = conn;
+  }
 
-	public String getlastId() {
-		String lastId = "MA000000";
-		try {
-			String query = "SELECT c.manager_id\r\n" + "FROM manager c\r\n"
-				+ "order by c.manager_id DESC\r\n" + "FETCH FIRST 1 ROWS ONLY";
-			ps = conn.prepareStatement(query);
-			ResultSet rs = ps.executeQuery();
-			if (rs.next())
-				lastId = rs.getString(1);
-			rs.close();
-			ps.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return lastId;
-	}
+  public String getlastId() {
+    String lastId = "MA000000";
+    try {
+      String query = "SELECT c.manager_id\r\n" + "FROM manager c\r\n"
+          + "order by c.manager_id DESC\r\n" + "FETCH FIRST 1 ROWS ONLY";
+      ps = conn.prepareStatement(query);
+      ResultSet rs = ps.executeQuery();
+      if (rs.next())
+        lastId = rs.getString(1);
+      rs.close();
+      ps.close();
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return lastId;
+  }
 
-	public static String generateNextId(String lastId) {
-		int numPart = Integer.parseInt(lastId.substring(2)) + 1;
-		return String.format("MA%06d", numPart);
-	}
+  public static String generateNextId(String lastId) {
+    int numPart = Integer.parseInt(lastId.substring(2)) + 1;
+    return String.format("MA%06d", numPart);
+  }
 
-	public String INSERT(String Name, String Password, String Phone_number) throws IOException {
-		String id = null;
-		try {
-			id = generateNextId(getlastId());
-			sql = "INSERT INTO MANAGER VALUES(?, ?, ?, ?)";
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, id);
-			ps.setString(2, Name);
-			ps.setString(3, Password);
-			ps.setString(4, Phone_number);
+  public String INSERT(String Name, String Password, String Phone_number) {
+    String id = null;
+    try {
+      id = generateNextId(getlastId());
+      sql = "INSERT INTO MANAGER VALUES(?, ?, ?, ?)";
+      ps = conn.prepareStatement(sql);
+      ps.setString(1, id);
+      ps.setString(2, Name);
+      ps.setString(3, Password);
+      ps.setString(4, Phone_number);
 
-			int res = ps.executeUpdate();
-			/*
-			if (res == 0) {
-			  System.out.println("Can't insert");
-			} else {
-			  System.out.println(id + " manager insert");
-			}
-			*/
-			ps.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return id;
-	}
+      int res = ps.executeUpdate();
+      /*
+       * if (res == 0) { System.out.println("Can't insert"); } else { System.out.println(id +
+       * " manager insert"); }
+       */
+      ps.close();
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return id;
+  }
 
-	public int UPDATE(String id, String Attribute, String update_value) throws IOException {
-		int res = 0;
-		try {
-			sql = "UPDATE MANAGER " + "SET " + Attribute + " = ? " + "WHERE manager_id = ?";
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, update_value);
-			ps.setString(2, id);
+  public int UPDATE(String id, String Attribute, String update_value) {
+    int res = 0;
+    try {
+      sql = "UPDATE MANAGER " + "SET " + Attribute + " = ? " + "WHERE manager_id = ?";
+      ps = conn.prepareStatement(sql);
+      ps.setString(1, update_value);
+      ps.setString(2, id);
 
-			res = ps.executeUpdate();
-			/*
-			if (res == 0) {
-			  System.out.println("Can't update");
-			} else {
-			  System.out.println(id + " manager update");
-			  System.out.println(Attribute + ": " + update_value);
-			}
-			*/
-			ps.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return res;
-	}
+      res = ps.executeUpdate();
+      /*
+       * if (res == 0) { System.out.println("Can't update"); } else { System.out.println(id +
+       * " manager update"); System.out.println(Attribute + ": " + update_value); }
+       */
+      ps.close();
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return res;
+  }
 
-	public int DELETE(String id) throws IOException {
-		int res = 0;
-		try {
-			sql = "DELETE FROM MANAGER " + "WHERE manager_id = ?";
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, id);
+  public int DELETE(String id) {
+    int res = 0;
+    try {
+      sql = "DELETE FROM MANAGER " + "WHERE manager_id = ?";
+      ps = conn.prepareStatement(sql);
+      ps.setString(1, id);
 
-			res = ps.executeUpdate();
-			/*
-			if (res == 0) {
-			  System.out.println("Can't delete");
-			} else {
-			  System.out.println(id + " manager delete");
-			}
-			*/
-			ps.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return res;
-	}
+      res = ps.executeUpdate();
+      /*
+       * if (res == 0) { System.out.println("Can't delete"); } else { System.out.println(id +
+       * " manager delete"); }
+       */
+      ps.close();
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return res;
+  }
+
+  public ResultSet SELECT(String id, String password) {
+    ResultSet res = null;
+    try {
+      sql = "SELECT * FROM MANAGER WHERE manager_id = ? AND password = ?";
+      ps = conn.prepareStatement(sql);
+      ps.setString(1, id);
+      ps.setString(2, password);
+
+      res = ps.executeQuery();
+      /*
+       * if (res == 0) { System.out.println("Can't delete"); } else { System.out.println(id +
+       * " customer delete"); }
+       */
+
+      ps.close();
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return res;
+  }
 }
+
