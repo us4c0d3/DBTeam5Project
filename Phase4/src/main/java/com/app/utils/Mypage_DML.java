98b896ba4c53c0f89446ad95ba6047f356da783f
SaebomYoon <dhsmf0204@gmail.com>
2023-11-26 14:32:32 +0900

    수정: 클래스 명, 메소드명 카멜케이스로 변경

--- Phase4/src/main/java/com/app/utils/Mypage_DML.java
+++ Phase4/src/main/java/com/app/utils/Mypage_DML.java
@@ -6,52 +6,52 @@ import java.sql.ResultSet;
 
 import com.app.tables.TableMain;
 
-public class Mypage_DML {
+public class MypageDML {
 	TableMain tm;
 	private Connection conn = null;
 	PreparedStatement ps;
 
-	public Mypage_DML(Connection conn) {
+	public MypageDML(Connection conn) {
 		tm = new TableMain();
 		this.conn = conn;
 	}
 
-	public ResultSet Customer_show(String id, String password) {
+	public ResultSet CustomerShow(String id, String password) {
 		ResultSet rs = null;
 		rs = tm.customer.SELECT(id, password);
 
 		return rs;
 	}
 
-	public ResultSet Manager_show(String id, String password) {
+	public ResultSet ManagerShow(String id, String password) {
 		ResultSet rs = null;
 		rs = tm.manager.SELECT(id, password);
 
 		return rs;
 	}
 
-	public ResultSet Chef_show(String id) {
+	public ResultSet ChefShow(String id) {
 		ResultSet rs = null;
 		rs = tm.chef.SELECT(id);
 
 		return rs;
 	}
 
-	public int Customer_update(String id, String Attribute, String update_value) {
+	public int CustomerUpdate(String id, String Attribute, String update_value) {
 		int res = 0;
 		res = tm.customer.UPDATE(id, Attribute, update_value);
 
 		return res;
 	}
 
-	public int Manager_update(String id, String Attribute, String update_value) {
+	public int ManagerUpdate(String id, String Attribute, String update_value) {
 		int res = 0;
 		res = tm.manager.UPDATE(id, Attribute, update_value);
 
 		return res;
 	}
 
-	public int Cjef_update(String id, String Attribute, String update_value) {
+	public int ChefUpdate(String id, String Attribute, String update_value) {
 		int res = 0;
 		res = tm.chef.UPDATE(id, Attribute, update_value);
 
