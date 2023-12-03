a9c3d61448132d6321a63b1832b84695b8f712ce
lnsqj123 <holmane33@gmail.com>
2023-12-03 14:43:54 +0900

    fix: 포맷터에 맞게 코드 변경

--- Phase4/src/main/java/com/app/tables/Menu.java
+++ Phase4/src/main/java/com/app/tables/Menu.java
@@ -11,165 +11,163 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
-  Connection conn;
-  String sql = "";
-  PreparedStatement ps;
-  private String menu_id;
-  private Date start_date;
-  private Date end_date;
+	Connection conn;
+	String sql = "";
+	PreparedStatement ps;
+	private String menu_id;
+	private Date start_date;
+	private Date end_date;
 
-  public Menu(Connection conn) {
-    this.conn = conn;
-  }
+	public Menu(Connection conn) {
+		this.conn = conn;
+	}
 
-  public Menu(String menu_id, Date start_date, Date end_date) {
-    this.menu_id = menu_id;
-    this.start_date = start_date;
-    this.end_date = end_date;
-  }
+	public Menu(String menu_id, Date start_date, Date end_date) {
+		this.menu_id = menu_id;
+		this.start_date = start_date;
+		this.end_date = end_date;
+	}
 
-  public String getMenu_id() {
-    return this.menu_id;
-  }
+	public String getMenu_id() {
+		return this.menu_id;
+	}
 
-  public Date getStart_date() {
-    return this.start_date;
-  }
+	public Date getStart_date() {
+		return this.start_date;
+	}
 
-  public Date getEnd_date() {
-    return this.end_date;
-  }
+	public Date getEnd_date() {
+		return this.end_date;
+	}
 
-  public void setMenu_id(String menu_id) {
-    this.menu_id = menu_id;
-  }
+	public void setMenu_id(String menu_id) {
+		this.menu_id = menu_id;
+	}
 
-  public void setStart_date(Date start_date) {
-    this.start_date = start_date;
-  }
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
 
-  public void setEnd_date(Date end_date) {
-    this.end_date = end_date;
-  }
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
 
-  public String getlastId() {
-    String lastId = "ME000000";
-    try {
-      String query = "SELECT c.menu_id\r\n" + "FROM menu c\r\n" + "order by c.menu_id DESC\r\n"
-          + "FETCH FIRST 1 ROWS ONLY";
-      ps = conn.prepareStatement(query);
-      ResultSet rs = ps.executeQuery();
-      if (rs.next())
-        lastId = rs.getString(1);
-      rs.close();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return lastId;
-  }
+	public String getlastId() {
+		String lastId = "ME000000";
+		try {
+			String query = "SELECT c.menu_id\r\n" + "FROM menu c\r\n" + "order by c.menu_id DESC\r\n"
+				+ "FETCH FIRST 1 ROWS ONLY";
+			ps = conn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next())
+				lastId = rs.getString(1);
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lastId;
+	}
 
-  public static String generateNextId(String lastId) {
-    int numPart = Integer.parseInt(lastId.substring(2)) + 1;
-    return String.format("ME%06d", numPart);
-  }
+	public static String generateNextId(String lastId) {
+		int numPart = Integer.parseInt(lastId.substring(2)) + 1;
+		return String.format("ME%06d", numPart);
+	}
 
+	public String INSERT(String start_date, String end_date) throws IOException {
+		String id = "";
+		try {
+			id = generateNextId(getlastId());
+			sql = "INSERT INTO MENU VALUES(?, TO_DATE(?, 'yyyy-mm-dd'), TO_DATE(?, 'yyyy-mm-dd'))";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, id);
+			ps.setString(2, start_date);
+			ps.setString(3, end_date);
 
-  public String INSERT(String start_date, String end_date) throws IOException {
-    String id = "";
-    try {
-      id = generateNextId(getlastId());
-      sql = "INSERT INTO MENU VALUES(?, TO_DATE(?, 'yyyy-mm-dd'), TO_DATE(?, 'yyyy-mm-dd'))";
-      ps = conn.prepareStatement(sql);
-      ps.setString(1, id);
-      ps.setString(2, start_date);
-      ps.setString(3, end_date);
+			int res = ps.executeUpdate();
+			// conn.commit();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return id;
+	}
 
-      int res = ps.executeUpdate();
-      // conn.commit();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return id;
-  }
+	public int UPDATE(String id, String attribute, String update_value) throws IOException {
+		int res = 0;
+		try {
+			sql = "UPDATE MENU " + "SET " + attribute + " = ? " + "WHERE Menu_id = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, update_value);
+			ps.setString(2, id);
 
-  public int UPDATE(String id, String attribute, String update_value) throws IOException {
-    int res = 0;
-    try {
-      sql = "UPDATE MENU " + "SET " + attribute + " = ? " + "WHERE Menu_id = ?";
-      ps = conn.prepareStatement(sql);
-      ps.setString(1, update_value);
-      ps.setString(2, id);
+			res = ps.executeUpdate();
+			// if (res == 0) {
+			// System.out.println("Can't update");
+			// }
+			// else {
+			// System.out.println(id + " Menu update");
+			// System.out.println(attribute + ": " + update_value);
+			// }
 
-      res = ps.executeUpdate();
-      // if (res == 0) {
-      // System.out.println("Can't update");
-      // }
-      // else {
-      // System.out.println(id + " Menu update");
-      // System.out.println(attribute + ": " + update_value);
-      // }
+			// conn.commit();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
+	}
 
-      // conn.commit();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return res;
-  }
+	public List<Menu> SELECT(String start_date, String end_date) throws IOException { // start_date,
+																						// end_date로
+																						// tuple 추출
+		List<Menu> Menus = new ArrayList<>();
+		ResultSet rs = null;
+		try {
+			sql = "SELECT * FROM Menu WHERE start_date = TO_DATE(?, 'yyyy-mm-dd') AND end_date = TO_DATE(?, 'yyyy-mm-dd')";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, start_date);
+			ps.setString(2, end_date);
 
-  public List<Menu> SELECT(String start_date, String end_date) throws IOException { // start_date,
-                                                                                    // end_date로
-                                                                                    // tuple 추출
-    List<Menu> Menus = new ArrayList<>();
-    ResultSet rs = null;
-    try {
-      sql =
-          "SELECT * FROM Menu WHERE start_date = TO_DATE(?, 'yyyy-mm-dd') AND end_date = TO_DATE(?, 'yyyy-mm-dd')";
-      ps = conn.prepareStatement(sql);
-      ps.setString(1, start_date);
-      ps.setString(2, end_date);
+			rs = ps.executeQuery();
+			if (rs != null) {
+				while (rs.next()) {
+					Menus.add(new Menu(rs.getString(1), rs.getDate(2), rs.getDate(3)));
+				}
+			}
+			// conn.commit();
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Menus;
+	}
 
-      rs = ps.executeQuery();
-      if (rs != null) {
-        while (rs.next()) {
-          Menus.add(new Menu(rs.getString(1), rs.getDate(2), rs.getDate(3)));
-        }
-      }
-      // conn.commit();
-      rs.close();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return Menus;
-  }
+	public List<Menu> SELECT2() throws IOException { // 모든 tuple 추출
+		List<Menu> Menus = new ArrayList<>();
+		ResultSet rs = null;
+		try {
+			sql = "SELECT * FROM Menu ORDER BY start_date";
+			ps = conn.prepareStatement(sql);
 
-  public List<Menu> SELECT2() throws IOException { // 모든 tuple 추출
-    List<Menu> Menus = new ArrayList<>();
-    ResultSet rs = null;
-    try {
-      sql = "SELECT * FROM Menu ORDER BY start_date";
-      ps = conn.prepareStatement(sql);
-
-      rs = ps.executeQuery();
-      if (rs != null) {
-        while (rs.next()) {
-          Menus.add(new Menu(rs.getString(1), rs.getDate(2), rs.getDate(3)));
-        }
-      }
-      // conn.commit();
-      rs.close();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return Menus;
-  }
+			rs = ps.executeQuery();
+			if (rs != null) {
+				while (rs.next()) {
+					Menus.add(new Menu(rs.getString(1), rs.getDate(2), rs.getDate(3)));
+				}
+			}
+			// conn.commit();
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Menus;
+	}
 }
--- Phase4/src/main/java/com/app/tables/Menu_item.java
+++ Phase4/src/main/java/com/app/tables/Menu_item.java
@@ -10,178 +10,176 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menu_item {
-  Connection conn;
-  String sql = "";
-  PreparedStatement ps;
+	Connection conn;
+	String sql = "";
+	PreparedStatement ps;
 
-  private String item_id, name;
-  private int unit_price, item_quantity;
-  private String category, soldout;
+	private String item_id, name;
+	private int unit_price, item_quantity;
+	private String category, soldout;
 
-  public Menu_item(Connection conn) {
-    this.conn = conn;
-  }
+	public Menu_item(Connection conn) {
+		this.conn = conn;
+	}
 
-  public Menu_item(String item_id, String name, int unit_price, int item_quantity, String category,
-      String soldout) {
-    this.item_id = item_id;
-    this.name = name;
-    this.unit_price = unit_price;
-    this.item_quantity = item_quantity;
-    this.category = category;
-    this.soldout = soldout;
-  }
+	public Menu_item(String item_id, String name, int unit_price, int item_quantity, String category,
+		String soldout) {
+		this.item_id = item_id;
+		this.name = name;
+		this.unit_price = unit_price;
+		this.item_quantity = item_quantity;
+		this.category = category;
+		this.soldout = soldout;
+	}
 
-  public String getItem_id() {
-    return this.item_id;
-  }
+	public String getItem_id() {
+		return this.item_id;
+	}
 
-  public String getName() {
-    return this.name;
-  }
+	public String getName() {
+		return this.name;
+	}
 
-  public int getUnit_price() {
-    return this.unit_price;
-  }
+	public int getUnit_price() {
+		return this.unit_price;
+	}
 
-  public int getItem_quantity() {
-    return this.item_quantity;
-  }
+	public int getItem_quantity() {
+		return this.item_quantity;
+	}
 
-  public String getCategory() {
-    return this.category;
-  }
+	public String getCategory() {
+		return this.category;
+	}
 
-  public String getSoldout() {
-    return this.soldout;
-  }
+	public String getSoldout() {
+		return this.soldout;
+	}
 
-  public void setItem_id(String item_id) {
-    this.item_id = item_id;
-  }
+	public void setItem_id(String item_id) {
+		this.item_id = item_id;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public void setUnit_price(int unit_price) {
-    this.unit_price = unit_price;
-  }
+	public void setUnit_price(int unit_price) {
+		this.unit_price = unit_price;
+	}
 
-  public void setItem_quantity(int item_quantity) {
-    this.item_quantity = item_quantity;
-  }
+	public void setItem_quantity(int item_quantity) {
+		this.item_quantity = item_quantity;
+	}
 
-  public void setCategory(String category) {
-    this.category = category;
-  }
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
-  public void setSoldout(String soldout) {
-    this.soldout = soldout;
-  }
+	public void setSoldout(String soldout) {
+		this.soldout = soldout;
+	}
 
-  public String getlastId() {
-    String lastId = "IT000000";
-    try {
-      String query = "SELECT mi.Item_id\r\n" + "FROM Menu_item mi\r\n"
-          + "order by mi.Item_id DESC\r\n" + "FETCH FIRST 1 ROWS ONLY";
-      ps = conn.prepareStatement(query);
-      ResultSet rs = ps.executeQuery();
-      if (rs.next())
-        lastId = rs.getString(1);
-      rs.close();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-      System.exit(1);
-    }
-    return lastId;
-  }
+	public String getlastId() {
+		String lastId = "IT000000";
+		try {
+			String query = "SELECT mi.Item_id\r\n" + "FROM Menu_item mi\r\n"
+				+ "order by mi.Item_id DESC\r\n" + "FETCH FIRST 1 ROWS ONLY";
+			ps = conn.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next())
+				lastId = rs.getString(1);
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return lastId;
+	}
 
-  public static String generateNextId(String lastId) {
-    int numPart = Integer.parseInt(lastId.substring(2)) + 1;
-    return String.format("IT%06d", numPart);
-  }
+	public static String generateNextId(String lastId) {
+		int numPart = Integer.parseInt(lastId.substring(2)) + 1;
+		return String.format("IT%06d", numPart);
+	}
 
-  public String INSERT(String name, String category, int Unit_price) throws IOException {
-    String id = null;
-    try {
-      id = generateNextId(getlastId());
+	public String INSERT(String name, String category, int Unit_price) throws IOException {
+		String id = null;
+		try {
+			id = generateNextId(getlastId());
 
-      sql =
-          "INSERT INTO Menu_item(Item_id, Name, Category, Unit_price, Item_quantity) VALUES(?, ?, ?, ?, 0)";
-      ps = conn.prepareStatement(sql);
-      ps.setString(1, id);
-      ps.setString(2, name);
-      ps.setString(3, category);
-      ps.setInt(4, Unit_price);
+			sql = "INSERT INTO Menu_item(Item_id, Name, Category, Unit_price, Item_quantity) VALUES(?, ?, ?, ?, 0)";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, id);
+			ps.setString(2, name);
+			ps.setString(3, category);
+			ps.setInt(4, Unit_price);
 
-      int res = ps.executeUpdate();
-      // conn.commit();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return id;
-  }
+			int res = ps.executeUpdate();
+			// conn.commit();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return id;
+	}
 
-  public List<Menu_item> SELECT(String name, String category, String soldout) throws IOException {
-    List<Menu_item> Menu_items = new ArrayList<>();
-    ResultSet rs = null;
-    try {
-      sql = "SELECT * FROM Menu_item WHERE name LIKE ? AND category LIKE ? AND soldout LIKE ?";
-      ps = conn.prepareStatement(sql);
-      ps.setString(1, "%" + name + "%");
-      ps.setString(2, "%" + category + "%");
-      if (soldout.length() == 0)
-        ps.setString(3, "_");
-      else {
-        ps.setString(3, soldout);
-      }
-      rs = ps.executeQuery();
-      if (rs != null) {
-        while (rs.next()) {
-          Menu_items.add(new Menu_item(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getInt(4),
-              rs.getString(5), rs.getString(6)));
-        }
-      }
-      // conn.commit();
-      rs.close();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return Menu_items;
-  }
+	public List<Menu_item> SELECT(String name, String category, String soldout) throws IOException {
+		List<Menu_item> Menu_items = new ArrayList<>();
+		ResultSet rs = null;
+		try {
+			sql = "SELECT * FROM Menu_item WHERE name LIKE ? AND category LIKE ? AND soldout LIKE ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, "%" + name + "%");
+			ps.setString(2, "%" + category + "%");
+			if (soldout.length() == 0)
+				ps.setString(3, "_");
+			else {
+				ps.setString(3, soldout);
+			}
+			rs = ps.executeQuery();
+			if (rs != null) {
+				while (rs.next()) {
+					Menu_items.add(new Menu_item(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getInt(4),
+						rs.getString(5), rs.getString(6)));
+				}
+			}
+			// conn.commit();
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Menu_items;
+	}
 
-  public List<Menu_item> SELECT2(String menu_id) throws IOException {
-    List<Menu_item> Menu_items = new ArrayList<>();
-    ResultSet rs = null;
-    try {
-      sql =
-          "SELECT mi.item_id, mi.name, mi.unit_price, mi.item_quantity, mi.category, mi.soldout\r\n"
-              + "FROM menu me, menu_item mi, contains co\r\n" + "WHERE me.menu_id = ?\r\n"
-              + "AND me.menu_id = co.menu_id\r\n" + "AND mi.item_id = co.item_id";
-      ps = conn.prepareStatement(sql);
-      ps.setString(1, menu_id);
-      rs = ps.executeQuery();
-      if (rs != null) {
-        while (rs.next()) {
-          Menu_items.add(new Menu_item(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getInt(4),
-              rs.getString(5), rs.getString(6)));
-        }
-      }
-      // conn.commit();
-      rs.close();
-      ps.close();
-    } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return Menu_items;
-  }
+	public List<Menu_item> SELECT2(String menu_id) throws IOException {
+		List<Menu_item> Menu_items = new ArrayList<>();
+		ResultSet rs = null;
+		try {
+			sql = "SELECT mi.item_id, mi.name, mi.unit_price, mi.item_quantity, mi.category, mi.soldout\r\n"
+				+ "FROM menu me, menu_item mi, contains co\r\n" + "WHERE me.menu_id = ?\r\n"
+				+ "AND me.menu_id = co.menu_id\r\n" + "AND mi.item_id = co.item_id";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, menu_id);
+			rs = ps.executeQuery();
+			if (rs != null) {
+				while (rs.next()) {
+					Menu_items.add(new Menu_item(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getInt(4),
+						rs.getString(5), rs.getString(6)));
+				}
+			}
+			// conn.commit();
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Menu_items;
+	}
 
 }
