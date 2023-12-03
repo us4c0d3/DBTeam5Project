44744433b823216c48ed71cc32341d4a90ebb08d
lnsqj123 <holmane33@gmail.com>
2023-12-04 02:08:57 +0900

    fix: toggleLoginLogout에서 파일 위치에 따라 href부분을 조정

--- Phase4/src/main/webapp/static/js/navbarScript.js
+++ Phase4/src/main/webapp/static/js/navbarScript.js
@@ -1,37 +1,51 @@
 function getUserIdFromCookie() {
-    let cookies = document.cookie.split(';');
-    for(let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i].trim().split('=');
-        if(cookie[0] === 'userId') return cookie[1];
-    }
-    return null;
+	let cookies = document.cookie.split(';');
+	for (let i = 0; i < cookies.length; i++) {
+		let cookie = cookies[i].trim().split('=');
+		if (cookie[0] === 'userId') return cookie[1];
+	}
+	return null;
 }
 
 function toggleLoginLogout() {
-    let userId = getUserIdFromCookie();
-    if(userId) {
-        document.querySelector('.nav-link[href="./Login.html"]').innerHTML = '<span class="navbar-brand mb-0 h">Logout</span>';
-    } else {
-        document.querySelector('.nav-link[href="./Login.html"]').innerHTML = '<span class="navbar-brand mb-0 h">Login</span>';
-    }
+	let userId = getUserIdFromCookie();
+	if (userId) {
+		var currentUrl = window.location.href;
+		var fileExtension = currentUrl.split('.').pop();
+		if (fileExtension === 'html') {
+			document.querySelector('.nav-link[href="./Login.html"]').innerHTML = '<span class="navbar-brand mb-0 h">Logout</span>';
+		}
+		else {
+			document.querySelector('.nav-link[href="./../static/html/Login.html"]').innerHTML = '<span class="navbar-brand mb-0 h">Logout</span>';
+		}
+	} else {
+		var currentUrl = window.location.href;
+		var fileExtension = currentUrl.split('.').pop();
+		if (fileExtension === 'html') {
+			document.querySelector('.nav-link[href="./Login.html"]').innerHTML = '<span class="navbar-brand mb-0 h">Login</span>';
+		}
+		else {
+			document.querySelector('.nav-link[href="./../static/html/Login.html"]').innerHTML = '<span class="navbar-brand mb-0 h">Login</span>';
+		}
+	}
 }
 
 function toggleMenuItems() {
-    let userId = getUserIdFromCookie();
-    if(userId) {
-        let isManager = userId.startsWith('MA');
-        let menuItems = document.querySelectorAll('.nav li');
-        menuItems.forEach(item => {
-            if(isManager || item.classList.contains('ShowMenu') || item.classList.contains('ShowMenu_item')) {
-                item.style.display = '';
-            } else {
-                item.style.display = 'none';
-            }
-        });
-    }
+	let userId = getUserIdFromCookie();
+	if (userId) {
+		let isManager = userId.startsWith('MA');
+		let menuItems = document.querySelectorAll('.nav li');
+		menuItems.forEach(item => {
+			if (isManager || item.classList.contains('ShowMenu') || item.classList.contains('ShowMenu_item')) {
+				item.style.display = '';
+			} else {
+				item.style.display = 'none';
+			}
+		});
+	}
 }
 
 window.onload = function() {
-    toggleLoginLogout();
-    toggleMenuItems();
+	toggleLoginLogout();
+	toggleMenuItems();
 };
