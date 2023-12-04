84996bdb20a142f1f6c8f8645af20ce2a52d3a8b
us4c0d3 <jwseok312@gmail.com>
2023-12-04 22:54:25 +0900

    feat: logout 기능 추가

--- Phase4/src/main/webapp/static/js/navbarScript.js
+++ Phase4/src/main/webapp/static/js/navbarScript.js
@@ -1,3 +1,12 @@
+function deleteCookie(name) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function handleLogout() {
+    deleteCookie('userId');
+    window.location.reload(); // 또는 로그인 페이지로 리디렉션: window.location.href = './Login.html';
+}
+
 function getUserIdFromCookie() {
     let cookies = document.cookie.split(';');
     for(let i = 0; i < cookies.length; i++) {
@@ -9,10 +18,13 @@ function getUserIdFromCookie() {
 
 function toggleLoginLogout() {
     let userId = getUserIdFromCookie();
+    let loginLink = document.querySelector('.nav-link[href="./Login.html"]');
     if(userId) {
-        document.querySelector('.nav-link[href="./Login.html"]').innerHTML = '<span class="navbar-brand mb-0 h">Logout</span>';
+        loginLink.innerHTML = '<span class="navbar-brand mb-0 h">Logout</span>';
+        loginLink.addEventListener('click', handleLogout); // 로그아웃 이벤트 핸들러 추가
     } else {
-        document.querySelector('.nav-link[href="./Login.html"]').innerHTML = '<span class="navbar-brand mb-0 h">Login</span>';
+        loginLink.innerHTML = '<span class="navbar-brand mb-0 h">Login</span>';
+        loginLink.removeEventListener('click', handleLogout); // 이전 이벤트 핸들러 제거
     }
 }
 
